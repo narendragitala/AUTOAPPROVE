b9cad9f45d28a52aec318719025335c27982ef7e
𝗡𝗔𝗥𝗨❥♡𝗝𝗔𝗔𝗧 <151159508+narendragitala@users.noreply.github.com>
2024-03-20 14:05:57 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -4,9 +4,9 @@ class Config:
     API_ID = int(getenv("API_ID", ""))
     API_HASH = getenv("API_HASH", "")
     BOT_TOKEN = getenv("BOT_TOKEN", "")
-    FSUB = getenv("FSUB", "Venom_Chatz")
+    FSUB = getenv("FSUB", "SHAYRIGALIBKI")
     CHID = int(getenv("CHID", ""))
-    SUDO = list(map(int, getenv("SUDO", "5822700831").split()))
+    SUDO = list(map(int, getenv("SUDO", "5473771943").split()))
     MONGO_URI = getenv("MONGO_URI", "")
     
 cfg = Config()
